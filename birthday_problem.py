5120400d8b7b9676b51e2a48935708a08c6c067d
Samuel Todd <samueldtodd@gmail.com>
2022-02-03 17:05:37 +0000

    Create birthday_problem.py

---
+++ birthday_problem.py
@@ -0,0 +1,20 @@
+import random
+import statistics
+
+probability = []
+lengths = []
+
+
+for simulations in range(0, 100):
+	birthdays = []
+	for people in range(0,365):
+		birthday = random.randint(0,365)
+		if birthday not in birthdays:
+			birthdays.append(birthday)
+		else:
+			birthdays.append(birthday)
+			lengths.append(len(birthdays))
+			break
+
+print(lengths)		
+
