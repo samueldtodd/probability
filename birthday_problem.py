afa1ee49ed5839bffeebf9a9a8f9a904b8b7099c
Samuel Todd <samueldtodd@gmail.com>
2022-02-05 18:23:48 +0000

    Update birthday_problem.py

--- birthday_problem.py
+++ birthday_problem.py
@@ -1,9 +1,13 @@
+#How many people must be gathered together in a room, before you can be certain 
+#that there is a greater than 50/50 chance that at least two of them have the same birthday?
+
 import random
 import statistics
 
+simulation_amount = 100000
 lengths = []
 
-for simulations in range(0, 100000):
+for simulations in range(0, simulation_amount):
 	birthdays = []
 	for people in range(0,365):
 		birthday = random.randint(0,365)
@@ -14,4 +18,4 @@ for simulations in range(0, 100000):
 			lengths.append(len(birthdays))
 			break
 
-print(statistics.median(lengths))
+print("After", simulation_amount, "simulations, an average of", (statistics.median(lengths)), "people entered the room before two shared a birthday.")
