e8f2cb736515008194f3e249265e6231e1f639dd
Samuel Todd <samueldtodd@gmail.com>
2022-06-22 07:47:38 +0100

    implemented oop

--- dice_roll.py
+++ dice_roll.py
@@ -1,14 +1,31 @@
 import random
 
-results = []
+class Die():
+
+	def __init__(self, sides = 6):
+		self.sides = sides
+
+	def roll_die(self):
+		roll = random.randint(1, self.sides)
+		return roll
+
+win = 0.0
+lose = 0.0
+
+d1 = Die()
+d2 = Die()
 
 for roll in range(0, 100000):
-	dice_one = random.randint(1,6)
-	dice_two = random.randint(1,6)
-	if (dice_one > 4) | (dice_two > 4):
-		results.append('Lose')
-	else:
-		results.append('Win')
 
-print(results.count('Win') / (results.count('Lose') + results.count('Win')))
+	d1r = d1.roll_die()
+	d2r = d2.roll_die()
+
+	if (d1r > 4) | (d2r > 4):
+		lose += 1
+	else:
+		win += 1
+
+print(win / (win + lose))
+
+print(4.0/6) * (4.0/6)
 
